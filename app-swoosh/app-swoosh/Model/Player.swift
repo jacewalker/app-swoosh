42d8cb328dd55648716880eef0d02dc2a0a92cdf
jace <jacewalker@me.com>
2017-09-28 17:27:42 +1000

    next button works only when option is selected

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jace Walker on 28/9/17.
+//  Copyright © 2017 Jace Walker. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
